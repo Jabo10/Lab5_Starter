a3070060f9a0d601b5a3f0e92f5ab0a33ce29a59
Jabo10 <jabondano@ucsd.edu>
2024-05-02 12:59:12 -0700

    tests completed

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,14 @@
 // sum.test.js
 
-test('adds 1 + 2 to equal 3', () => {
-  // TODO
+// Import the sum function from the file where it's defined
+import { sum } from '../code-to-unit-test/sum';
+
+// Test using the JavaScript addition operator
+test('adds 1 + 2 to equal 3 using basic addition', () => {
+    expect(1 + 2).toBe(3);
+});
+
+// Test using the sum function imported from sum.js
+test('adds 1 + 2 to equal 3 using the sum function', () => {
+    expect(sum(1, 2)).toBe(3);
 });
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,93 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+describe('isPhoneNumber', () => {
+  test('correct phone number format - ', () => {
+    expect(isPhoneNumber('999-999-9999')).toBe(true);
+  });
+
+  test(' correct phone number format () ', () => {
+    expect(isPhoneNumber('(999) 999-9999')).toBe(true);
+  });
+
+  test('false incomplete phone number', () => {
+    expect(isPhoneNumber('999-999')).toBe(false);
+  });
+
+  test('false non number string', () => {
+    expect(isPhoneNumber('the-the-thee')).toBe(false);
+  });
+});
+
+describe('isEmail', () => {
+  test('correct email format', () => {
+    expect(isEmail('email@email.com')).toBe(true);
+  });
+
+  test('correct email with numbers + underscore', () => {
+    expect(isEmail('email145@email.com')).toBe(true);
+  });
+
+  test('false email no domain', () => {
+    expect(isEmail('email@')).toBe(false);
+  });
+
+  test('false email without no @', () => {
+    expect(isEmail('emailemail.com')).toBe(false);
+  });
+});
+
+describe('isStrongPassword', () => {
+  test('correct strong password', () => {
+    expect(isStrongPassword('Z5v8n4f9')).toBe(true);
+  });
+
+  test('correct minimal password', () => {
+    expect(isStrongPassword('Zvn5')).toBe(true);
+  });
+
+  test('false too short', () => {
+    expect(isStrongPassword('Zv5')).toBe(false);
+  });
+
+  test('false not allowed special characters', () => {
+    expect(isStrongPassword('Z5v8n4#@!')).toBe(false);
+  });
+});
+
+describe('isDate', () => {
+  test('correct date format', () => {
+    expect(isDate('4/12/2000')).toBe(true);
+  });
+
+  test('correct date single digit day and month', () => {
+    expect(isDate('4/1/2000')).toBe(true);
+  });
+
+  test('false date format', () => {
+    expect(isDate('122/22/2000')).toBe(false);
+  });
+
+  test('false writen date', () => {
+    expect(isDate('April 12, 2000')).toBe(false);
+  });
+});
+
+describe('isHexColor', () => {
+  test('correct 6 character hex color', () => {
+    expect(isHexColor('#ffffff')).toBe(true);
+  });
+
+  test('correct 3 character hex color', () => {
+    expect(isHexColor('#fff')).toBe(true);
+  });
+
+  test('false hex color without #', () => {
+    expect(isHexColor('12345')).toBe(false);
+  });
+
+  test('false hex color', () => {
+    expect(isHexColor('#54321h')).toBe(false);
+  });
+});
+
